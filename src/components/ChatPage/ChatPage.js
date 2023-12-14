951fc4339d4175981750c4e945135bc77fb34b6c
Aslam Shikalgar <aslshika@WKMZTCC2A15E.global.publicisgroupe.net>
2023-12-14 18:09:23 +0530

    design updates

---
+++ src/components/ChatPage/ChatPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './ChatPage.module.scss';
+
+import logo from './../../assets/images/dummy-logo.png'
+import avatar from './../../assets/images/avatar.svg'
+
+import twitter from './../../assets/images/twitter.svg'
+import telegram from './../../assets/images/telegram.svg'
+import vector from './../../assets/images/vector.svg'
+
+import { Link } from "react-router-dom";
+
+
+const ChatPage = () => {
+    return (
+        <div className={styles.chat}>
+            <div className='bg-image'></div>
+            <div className={styles.header}>
+                <div>
+                    <Link to="/"><img src={logo} alt="Logo" /></Link>
+                </div>
+                <div>
+                    <img src={avatar} alt="avatar" className='' />
+                </div>
+                <div>
+                    <div className={styles.social}>
+                        <Link to="https://twitter.com/" target='_blank' > <img src={twitter} alt="Twitter" /> </Link>
+                        <Link to="https://web.telegram.org/" > <img src={telegram} alt="Telegram" /> </Link>
+                        <Link to="https://google.com" > <img src={vector} alt="logo a" /> </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ChatPage;
